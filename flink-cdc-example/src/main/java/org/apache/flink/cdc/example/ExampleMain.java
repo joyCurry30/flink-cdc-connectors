258eb6bf48687bf10db1f75df7aad6d9722a9321
Tianzhu Wen <curry30play@gmail.com>
2025-04-29 15:14:57 +0800

    stash.

--- flink-cdc-example/src/main/java/org/apache/flink/cdc/example/ExampleMain.java
+++ flink-cdc-example/src/main/java/org/apache/flink/cdc/example/ExampleMain.java
@@ -5,7 +5,7 @@ import org.apache.flink.cdc.cli.CliFrontend;
 public class ExampleMain {
     public static void main(String[] args) throws Exception {
         String jobPath = "/Users/lapata/clickzetta_jobs/flink-cdc-3.0/values_2_values.yml";
-        String[] extArgs = {jobPath, "-t", "yarn-application"};
+        String[] extArgs = {jobPath, "-t", "yarn-application", "--flink-home", "/Users/lapata/tool_box/flink-1.20.0"};
 
         CliFrontend.main(extArgs);
     }
